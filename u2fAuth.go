c5d5feffd62f81fcd8229eb89cd6ec2b0b4a662d
andela-dmigwi <migwi.ndungu@andela.com>
2017-05-16 12:16:35 +0300

    add dependency management.

--- u2fAuth.go
+++ u2fAuth.go
@@ -18,19 +18,25 @@ func login(w http.ResponseWriter, r *http.Request) {
 		// logic part of log in
 		fmt.Println("username:", reflect.TypeOf(r.Form["username"]))
 		fmt.Printf("password: %s", r.Form["password"][0])
-		http.Redirect(w, r, "/auth", 301)
+		appId := "http://localhost"
+
+		// Send registration request to the browser.
+		c, _ := NewChallenge(appId, []string{appId})
+		req, _ := c.RegisterRequest()
+
+		// Read response from the browser.
+		var resp RegisterResponse
+		challenge, err := Register(resp, c, nil)
+		if err != nil {
+			fmt.Println("Registration failed.")
+		}
+		temp, _ := template.ParseFiles("template/u2fAuth.html")
+		temp.Execute(w, challenge)
 	}
 }
 
-func u2f_auth(w http.ResponseWriter, r *http.Request) {
-	challenge := "6571^&6*&"
-	temp, _ = template.ParseFiles("template/u2f_auth.html")
-	temp.Execute(w, challenge)
-}
-
 func main() {
-	http.HandleFunc("/", login) // set router
-	http.HandleFunc("/auth", u2f_auth)
+	http.HandleFunc("/", login)              // set router
 	err := http.ListenAndServe(":5000", nil) // set listen port
 	fmt.Println("server running on port 5000")
 	if err != nil {
